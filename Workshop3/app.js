33f22b7edd4cf57707c81f08ef5c55093e6be88b
ilyas-elmarzak <57985894+ilyas-elmarzak@users.noreply.github.com>
2020-03-20 15:29:17 +0100

    Add files via upload

---
+++ Workshop3/app.js
@@ -0,0 +1,14 @@
+function nextInLine(){
+    var tab = [1,2,3,4,5,6];
+    console.log("Affichage du tableau avant le push :");
+    console.log("[" +tab +"]");
+    tab.push(8);
+    console.log("Affichage du tableau Aprés le push :");
+    console.log("[" +tab +"]");
+    var removeFirstname = tab.shift(); // remove le premier element du tableau 
+    // tab.splice(0)
+    console.log("Affichage du tableau après la suppression du  premier element : ");
+    console.log("[" +tab +"]");
+    console.log("Affichage du supprétion de la premier element : ");
+    console.log(removeFirstname);
+}nextInLine();
